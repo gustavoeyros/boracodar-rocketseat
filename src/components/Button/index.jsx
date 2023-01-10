1fc5d9b19b9498a962a92c55a871cfe26253ce40
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-10 19:37:27 -0300

    feat: continue or pause effect

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import { ButtonContainer } from "./styled";
 //icons
 import Play from "../../assets/play.svg";
 import PlayBack from "../../assets/play-back.svg";
 import Skip from "../../assets/play-forward.svg";
+import { IoMdPause } from "react-icons/io";
 
 const Button = () => {
+  const [isTouched, setIsTouched] = useState(false);
+
+  const touchedHandler = () => {
+    setIsTouched(!isTouched);
+  };
+  console.log(isTouched);
+
   return (
     <ButtonContainer>
       <img src={PlayBack} />
 
-      <img src={Play} />
+      {!isTouched && <img src={Play} onClick={touchedHandler} />}
+      {isTouched && isTouched && (
+        <IoMdPause
+          onClick={touchedHandler}
+          style={{
+            width: "28.81px",
+            height: " 28.81px",
+            cursor: "pointer",
+            color: "white",
+          }}
+        />
+      )}
 
       <img src={Skip} />
     </ButtonContainer>
