6f0283ef7ea68f1590efd557d97e59ac80a84f77
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-10 23:27:19 -0300

    feat: timer and progress bar interaction

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -7,17 +7,36 @@ import {
 } from "./styled";
 
 const ProgressBar = (props) => {
-  const [currentProgress, setCurrentProgress] = useState(10);
-  console.log(props.checked);
+  const [currentProgress, setCurrentProgress] = useState(0);
+  const [currentTimer, setCurrentTimer] = useState(0);
+  const [currentMinute, setCurrentMinute] = useState(0);
+
+  let second = 0;
+
   useEffect(() => {
     if (props.checked) {
       setTimeout(() => {
-        setCurrentProgress((prev) => (prev += 1));
-      }, 500);
+        setCurrentProgress((prev) => (prev += 0.52083333333333));
+        setCurrentTimer((prev) => {
+          if (prev >= 59) {
+            return (second += 1);
+          }
+          return (prev += 1);
+        });
+        if (currentTimer >= 59) {
+          setCurrentMinute((prev) => prev + 1);
+        }
+      }, 50);
+
+      if (currentMinute === 3 && currentTimer == 20) {
+        return (second += 1);
+      }
     } else {
-      setCurrentProgress(10);
+      setCurrentProgress((prev) => prev);
     }
-  }, [currentProgress, props.checked]);
+    console.log("Minuto:" + currentMinute);
+    console.log("Segundo:" + currentTimer);
+  }, [currentProgress, props.checked, currentMinute]);
 
   return (
     <WrapperProgress>
@@ -26,8 +45,12 @@ const ProgressBar = (props) => {
       </TotalProgress>
 
       <Timer>
+        <span>
+          {currentTimer < 10
+            ? `0${currentMinute}:0${currentTimer}`
+            : ` 0${currentMinute}:${currentTimer}`}
+        </span>
         <span>03:20</span>
-        <span>00:12</span>
       </Timer>
     </WrapperProgress>
   );
